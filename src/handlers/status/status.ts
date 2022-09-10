8e2ec3d7abe7912b20a1f1aee0d1936ba0d93b88
Tunnelblick <larsss1024@gmail.com>
2022-09-10 23:02:06 +0200

    status

---
+++ src/handlers/status/status.ts
@@ -0,0 +1,27 @@
+import { Client } from "discord.js";
+
+export default (client: Client) => {
+
+    const statusOptions = [
+        `v2 live | !help`,
+    ]
+
+    let i = 0
+
+    const updateStatus = () => {
+
+        // because of how discord api works this shit has to be seperated from setstatus which is balls
+        client.user?.setActivity(statusOptions[i], {type: 0})
+        
+        if(++i >= statusOptions.length){
+            i = 0
+        }
+        setTimeout(updateStatus, 1000 * 60 * 10)
+    }
+    updateStatus()
+}
+
+export const config = {
+    dbName: 'STATUS_CHANGER',
+    displayName: 'Status Changer'
+}
