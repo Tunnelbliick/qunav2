a32777c04fa1c773cc92b5f5e909edb845f071ce
Tunnelblick <larsss1024@gmail.com>
2022-09-11 15:12:38 +0200

    info command slash command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,21 +5,33 @@ export default {
 
     category: "osu!",
     aliases: ["info", "i"],
+    slash: "both",
     description: "Information about Quna",
 
-    callback: async ({ message, args, prefix }) => {
+    callback: async ({ message, args, prefix, interaction }) => {
+
+        let isSlash = true;
+
+        if (message != null && message.content.startsWith(prefix)) {
+            isSlash = false;
+        }
 
         let embed = new MessageEmbed()
             .setTitle("Quna information")
             .setDescription(
                 "**Yet another multipurpose (but mainly focused on osu!) Discord bot recode version 2.**\n\n" +
                 "If you have any issues, suggestions or questions join our discord! \n\n" +
+                "[Privacy Policy](https://github.com/Tunnelbliick/qunav2/blob/master/PRIVACY.md)\n " +
                 "[Discord](https://discord.gg/azPWUfSMm3)\n" +
                 "[GitHub](https://github.com/Tunnelbliick/qunav2)")
             .setColor(0x737df9)
 
 
-        message.reply({ embeds: [embed] });
+        if (isSlash) {
+            interaction.reply({ embeds: [embed] });
+        } else {
+            message.reply({ embeds: [embed] });
+        }
         return;
     }
 
