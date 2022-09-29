cd85578d52f479801e468e75b572750c295c2407
Arumizu <edisontan2002@yahoo.com>
2022-09-29 22:35:52 +0800

    apex and source(2)

---
+++ src/commands/old_commands/sauce.ts
@@ -0,0 +1,53 @@
+import { ICommand } from "wokcommands";
+import sagiri from "sagiri";
+import { MessageEmbed } from "discord.js";
+import { buildErrEmbed } from "../../functions/util/errorEmbed";
+const sauce = sagiri(`${process.env.saucenao}`, { results: 1 });
+
+export default {
+
+    category: "Art",
+    description: "Quna tries to find a source for your image!",
+
+    callback: ({ message }) => {
+
+        message.attachments.forEach(attachment => {
+            sauce(attachment.proxyURL).then(response => {
+                let data = response[0]
+
+                const embed = new MessageEmbed()
+                .setColor(0x737df9)
+                .setTitle("Sauce found!")
+                .addFields([
+                    {
+                        name: 'Similarity',
+                        value: data.similarity + `%`,
+                        inline: true
+                    },
+                    {
+                        name: 'Author',
+                        value: `${data.authorName}`,
+                        inline: true
+                    },
+                    {
+                        name: 'Website',
+                        value: `${data.site}`,
+                        inline: true
+                    },
+                    {
+                        name: 'URL',
+                        value: `${data.url}`,
+                        inline: false
+                    },
+                ])
+                .setImage(data.thumbnail)
+                message.channel.send({embeds: [embed]})
+            })
+            .catch((err: any) => {
+                buildErrEmbed(err, message)
+            })
+        })
+
+    }
+
+} as ICommand
