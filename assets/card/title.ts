cd15397d6c26e24e6edccdaa2e0be4fb9747aebc
Tunnelblick <larsss1024@gmail.com>
2022-10-03 20:33:14 +0200

    added titles

---
+++ assets/card/title.ts
@@ -0,0 +1,47 @@
+const aim_title = [
+    { mod: "nm", title: "Sergeant General" },
+    { mod: "hr", title: "Precision Biased" },
+    { mod: "hd", title: "Disappearing Senses" },
+    { mod: "dt", title: "Cultivatist at Work" },
+    { mod: "hdhr", title: "Out of Sight" },
+    { mod: "hddt", title: "Proud Estate Owner" },
+    { mod: "hddthr", title: "Essence of Aim" },
+
+    { mod: "ez", title: "Reading Maestro" },
+    { mod: "ezhd", title: "Sublime Masochism" },
+    { mod: "ezdt", title: "Extended Reach" },
+    { mod: "ezhddt", title: "Vanishing Point" },
+    { mod: "ezhthd", title: "Scrambling of Time" }
+];
+
+const speed_title = [
+    { mod: "nomod", title: "Lengthy Incident" },
+    { mod: "hr", title: "Hi-Speed Mechatron" },
+    { mod: "hd", title: "Flow of Time" },
+    { mod: "dt", title: "Breaking the Limit" },
+    { mod: "hdhr", title: "Finding Solace" },
+    { mod: "hddt", title: "Traversing Dimensions" },
+    { mod: "hddthr", title: "Speed Trap Maniac" },
+
+    { mod: "ez", title: "Circumvention of Momentum" },
+    { mod: "ezhd", title: "Consolidation Reimagined" },
+    { mod: "ezdt", title: "Abused Fortitude" },
+    { mod: "ezhddt", title: "Fading Sunshine" },
+    { mod: "ezhthd", title: "Excuse me?" }
+];
+
+const acc_title = [
+    { mod: "nomod", title: "Everlasting Cycle" },
+    { mod: "hr", title: "Metronomical Prodigy" },
+    { mod: "hd", title: "Pathing of Unseen Roads" },
+    { mod: "dt", title: "Fortitude of Patience" },
+    { mod: "hdhr", title: "The Circle Unveiled" },
+    { mod: "hddt", title: "Finite Tenacity" },
+    { mod: "hddthr", title: "Vivid Horizons" },
+
+    { mod: "ez", title: "Ousted Diminision" },
+    { mod: "ezhd", title: "Formal Instigation" },
+    { mod: "ezdt", title: "Sovereign Monocle" },
+    { mod: "ezhddt", title: "Red Herring" },
+    { mod: "ezhthd", title: "Epitomy of Cheese" },
+];
