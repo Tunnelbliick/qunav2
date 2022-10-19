134e2cffc3ec8af11afd7bdb79f2c2381e04c7d3
Tunnelblick <larsss1024@gmail.com>
2022-10-19 23:34:16 +0200

    fix leaderboard null pointer

--- src/api/pickem/leaderboard.ts
+++ src/api/pickem/leaderboard.ts
@@ -21,19 +21,22 @@ export async function leaderboard(message: any, interaction: any) {
 
     let owc_year: any = await owc.findOne({ url: current_tournament });
 
-    if(owc_year === null) {
+    if (owc_year === null) {
         await noPickEm(message, interaction);
         return;
     }
-    
+
     let user: any = await User.findOne({ discordid: await encrypt(discordid) });
 
-    if(checkIfUserExists(user, message, interaction)) {
+    if (checkIfUserExists(user, message, interaction)) {
         return
     }
 
+    let user_position = undefined;
     let registration: any = await pickemRegistration.findOne({ owc: owc_year.id, user: user.id });
-    let user_position = await pickemRegistration.find({ owc: owc_year.id, user: { $ne: user.id }, total_score: { $gte: registration.total_score } }).sort({ total_score: -1 }).count().exec() + 1;
+
+    if (registration !== null)
+        user_position = await pickemRegistration.find({ owc: owc_year.id, user: { $ne: user.id }, total_score: { $gte: registration.total_score } }).sort({ total_score: -1 }).count().exec() + 1;
     let top_20: any = await pickemRegistration.find({ owc: owc_year.id }).sort({ total_score: -1 }).limit(10).exec();
     let top_20_userids = top_20.map((top: any) => top.user.toString());
     let users: any = await User.find({ _id: { $in: top_20_userids } });
@@ -72,26 +75,28 @@ export async function leaderboard(message: any, interaction: any) {
         index++;
     })
 
-    let total_score = `${registration.total_score}`;
-    let index_string = `${user_position}`;
+    if (registration !== null) {
+        let total_score = `${registration.total_score}`;
+        let index_string = `${user_position}`;
 
-    switch (total_score.length) {
-        case 1:
-            total_score = total_score + "  ";
-            break;
-        case 2:
-            total_score = total_score + " ";
-            break;
+        switch (total_score.length) {
+            case 1:
+                total_score = total_score + "  ";
+                break;
+            case 2:
+                total_score = total_score + " ";
+                break;
+        }
+
+        switch (index_string.length) {
+            case 1:
+                index_string = index_string + " ";
+                break;
+        }
+
+        description += "\n";
+        description += `#${index_string} | ${total_score} | ${user.username}\n`
     }
-
-    switch (index_string.length) {
-        case 1:
-            index_string = index_string + " ";
-            break;
-    }
-
-    description += "\n";
-    description += `#${index_string} | ${total_score} | ${user.username}\n`
     description += "```";
 
     let embed = new MessageEmbed()
