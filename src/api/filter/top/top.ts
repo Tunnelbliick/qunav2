5b53326f1c469d8665652ce7b969096ecd90ef46
Tunnelbliick <larsss1024@gmail.com>
2022-12-10 02:50:02 +0100

    fix unranked top not beeing able to search

--- src/api/filter/top/top.ts
+++ src/api/filter/top/top.ts
@@ -11,8 +11,8 @@ export function topFilterAndSort(top: any, filterOptions: TopPlaysfilter) {
         top = top.filter((top: any) => {
 
             let artist = top.value.beatmapset.artist === undefined ? "" : top.value.beatmapset.artist.toLowerCase();
-            let title = top.value.beatmapset.title=== undefined ? "" : top.value.beatmapset.title.toLowerCase();
-            let version = top.value.beatmapset.version=== undefined ? "" : top.value.beatmapset.version.toLowerCase();
+            let title = top.value.beatmapset.title === undefined ? "" : top.value.beatmapset.title.toLowerCase();
+            let version = top.value.beatmapset.version === undefined ? "" : top.value.beatmapset.version.toLowerCase();
             let creator = top.value.beatmapset.creator === undefined ? "" : top.value.beatmapset.creator.toLowerCase();
 
             return artist.includes(filterOptions.search) || title.includes(filterOptions.search) || version.includes(filterOptions.search) || creator.includes(filterOptions.search)
--- src/api/unranked/top.ts
+++ src/api/unranked/top.ts
@@ -1,4 +1,5 @@
 import score from "../../models/score";
+import { getBeatmapFromCache } from "../osu/beatmap";
 
 export async function loadUnrankedTop(user_id: any, mode: any) {
 
@@ -15,9 +16,11 @@ export async function loadUnrankedTop(user_id: any, mode: any) {
         max_pp: { $lte: 3000 }
     }).sort({ pp: -1 }).limit(100).exec();
 
-    top100.forEach((top: any) => {
+    top100.forEach(async (top: any) => {
 
+        const beatmap: any = await getBeatmapFromCache(top.mapid, top.checksum);
         top.beatmap = { id: top.mapid, checksum: top.checksum }
+        top.beatmapset = beatmap;
 
         returnArray.push(top);
 
